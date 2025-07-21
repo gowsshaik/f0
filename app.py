52e9119405d6e2b451cc4f4a0843912070e98cb1
gowsshaik <67041035+gowsshaik@users.noreply.github.com>
2025-07-21 08:31:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 import pytz
 import holidays
 from flask import Flask, request, jsonify, render_template
-
+app = Flask(__name__)
 # Ticker List
 TICKERS = [
     "360ONE.NS", "ABB.NS", "ACC.NS", "APLAPOLLO.NS", "ADANIENT.NS", "ADANIPORTS.NS", "AMBUJACEM.NS",
